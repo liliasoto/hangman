0287498f568b8f39cd2f90811762e5899f7ba8a4
liliasoto <0.lilia.soto.0@gmail.com>
2024-04-28 10:28:50 -0600

    Agregué el contador de segundos

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,13 +1,32 @@
 import hangman from '../img/hangman.png';
 import "../css/main.css"
+//Aquí importo los hooks
+import { useEffect, useState } from 'react';
 
 export default function Welcome(){
+    //Inicializamos la variable count en 0 usando useState()
+    const [count, setCount] = useState(0) 
+    
+    //Agregamos el useEffect(), este va a actualizar la variable count cada segundo
+    //aumentandola en 1, el efecto se sincroniza cada que el componente se monta y se
+    //desincroniza cada que el componente se desmonta como podemos ver en las dependencias []
+    useEffect(() => {
+        const key= setInterval(()=>{
+          setCount(count=>count+1)
+        },1000);
+    
+        return () => {
+          clearInterval(key);
+        };
+      }, [])
+
     return(
         <>
             <div className="wrapper">
                 <h1>Welcome to Hangman Game!!</h1>
                 <h2>Classical game</h2>
                 <img src={hangman} alt='Hangman image'></img>
+                <p> You've been playing for {count} seconds</p>
             </div>
         </>
     );
